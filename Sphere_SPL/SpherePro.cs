f9fe9f7b6ab5e5a7fbff9672cede0b49ca7ab883
sirsplove <sirsplove@gmail.com>
2020-01-30 15:54:44 -0500

    Initial check-in of module Sphere_SPL

---
+++ Sphere_SPL/SpherePro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Sphere
+{
+	private double diameter;
+	public Sphere()
+	{
+		diameter = 2.0;
+	}
+
+	public Sphere(double diameter)
+	{
+		this.setDiameter(diameter);
+	}
+
+	public double getVolume()
+	{
+		return (4.0 / 3.0) * Math.PI * Math.Pow(diameter / 2, 3);
+	}
+
+	public double getSurfaceArea()
+	{
+		return 4.0 * Math.PI * Math.Pow(diameter / 2, 2);
+	}
+
+	public double getDiameter()
+	{
+		return diameter;
+	}
+
+	public void setDiameter(double diameter)
+	{
+		if (diameter < 2)
+			this.diameter = 2;
+		else
+			this.diameter = diameter;
+	}
+
+	public override String ToString()
+	{
+		return "Sphere [diameter=" + diameter + "]";
+	}
+}
---
+++ Sphere_SPL/Sphere_SPL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SpherePro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sphere sphere = new Sphere();
+            sphere.setDiameter(8.5);
+
+            Console.WriteLine("The spheres diameter is: " + sphere.getDiameter() + " inches");
+            Console.WriteLine("The surface area is: " + Math.Round(sphere.getSurfaceArea(), 2) + " square inches");
+            Console.WriteLine("The volume is: " + Math.Round(sphere.getVolume(), 2) + " cubic inches");
+            Console.WriteLine("The spheres information: " + sphere.ToString());
+        }
+    }
+}
